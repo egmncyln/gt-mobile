67affaea3893c40309fa69cf538de727505d093b
Egemen Ceylan <egemen.ceylan1@gmail.com>
2023-03-11 22:07:45 +0300

    Basic communication between web and mobile is completed

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'react-native';
+import { useRef } from 'react';
+import { StatusBar, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const webViewRef = useRef();
+
+  function onMessage(message) {
+    Alert.alert(`The Page at "React Native" says:`, message.nativeEvent.data, [
+      {
+        onPress: () => webViewRef.current.injectJavaScript(`alert('${message.nativeEvent.data}')`),
+      },
+    ]);
+  }
+
   return (
     <>
       <StatusBar barStyle={'default'} />
-      <WebView source={{ uri: 'http://10.0.2.2:3000/' }} />
+      <WebView ref={webViewRef} onMessage={onMessage} source={{ uri: 'http://10.0.2.2:3000/' }} />
     </>
   );
 }
